9800051290c8b41c33e14c66e43b82709104c08e
ni30kp <51078410+ni30kp@users.noreply.github.com>
2020-05-04 18:19:43 +0530

    Add files via upload

---
+++ model/ssdlite_mobilenet_v2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+#
+# Copyright (c) 2020 Marcin Sielski.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# ------------------------------------------------------------------------------
+
+# Download model from OpenVINO Model Zoo
+/opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name ssdlite_mobilenet_v2
+
+# Convert model to Intermediate Representation for MYRIAD device
+/opt/intel/openvino/deployment_tools/model_optimizer/mo_tf.py --input_model public/ssdlite_mobilenet_v2/ssdlite_mobilenet_v2_coco_2018_05_09/frozen_inference_graph.pb --data_type FP16 --input_shape [1,300,300,3] --reverse_input_channels --transformations_config /opt/intel/openvino/deployment_tools/model_optimizer/extensions/front/tf/ssd_v2_support.json --tensorflow_object_detection_api_pipeline_config public/ssdlite_mobilenet_v2/ssdlite_mobilenet_v2_coco_2018_05_09/pipeline.config --model_name ssdlite_mobilenet_v2
